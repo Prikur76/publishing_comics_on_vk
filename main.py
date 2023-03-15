6bde0ea775860da06390b74bef032023759ee729
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-03-15 18:02:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
-import requests
+import urllib
 
+import requests
 
 
 def main():
@@ -8,14 +9,4 @@ def main():
 
 
 if __name__ == '__main__':
-    # main()
-
-    images_path = 'files'
-    os.makedirs(images_path, exist_ok=True)
-
-    link = 'https://xkcd.com/info.0.json'  # /info.0.json
-
-    response = requests.get(url=link)
-    response.raise_for_status()
-    comic = response.json()
-    print(comic['img'])
+    main()

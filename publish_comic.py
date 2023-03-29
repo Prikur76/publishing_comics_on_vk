d41093e7fe57e57422b724383ad116df3b69b1f3
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-03-29 11:39:48 +0300

    Update publish_comic.py

--- publish_comic.py
+++ publish_comic.py
@@ -15,12 +15,10 @@ logger = logging.getLogger(__name__)
 def get_comics_count():
     """Возвращает количество комиксов"""
     xkcd_url = urljoin('https://xkcd.com/', '/info.0.json')
-    response = requests.get(url=xkcd_url)
+    response = requests.get(xkcd_url)
     response.raise_for_status()
-    if response:
-        comic = response.json()
-        return comic['num']
-    raise requests.exceptions.HTTPError('Request failed')
+    comic = response.json()
+    return comic['num']
 
 
 def get_random_comic_description():
@@ -28,22 +26,19 @@ def get_random_comic_description():
     comics_count = get_comics_count()
     comic_number = choice(range(1, comics_count, 1))
     xkcd_random_url = f'https://xkcd.com/{comic_number}/info.0.json'
-    response = requests.get(url=xkcd_random_url)
+    response = requests.get(xkcd_random_url)
     response.raise_for_status()
-    if response:
-        comic = response.json()
-        image = urlsplit(comic['img'])
-        image_name = os.path.split(
-            unquote(image.path, encoding='utf-8', errors='replace')
-        )
-        comic_description = [comic['img'], comic['alt'], image_name[1]]
-        return comic_description
-    raise requests.exceptions.HTTPError('Request failed')
+    comic = response.json()
+    image = urlsplit(comic['img'])
+    image_name = os.path.split(
+        unquote(image.path, encoding='utf-8', errors='replace')
+    )
+    return comic['img'], comic['alt'], image_name[1]
 
 
 def fetch_image_from_url(image_link, image_directory, image_name):
     """Скачивает картинку с интернета по ссылке (GET-запрос)"""
-    response = requests.get(url=image_link)
+    response = requests.get(image_link)
     response.raise_for_status()
     image_path = os.path.join(
         os.path.abspath(image_directory), image_name
@@ -75,15 +70,23 @@ def main():
         os.makedirs(images_path)
 
     try:
-        comic = get_random_comic_description()
-        comic_url, comic_alt, comic_name = comic
+        comic_url, comic_alt, comic_name = get_random_comic_description()
         comic_path = os.path.join(images_path, comic_name)
+
         fetch_image_from_url(
             comic_url, images_path, comic_name
         )
-        attachments = vk.get_attachments_for_publish(
-            vk_token, vk_api_version, group_id, comic_path
+
+        upload_url = vk.get_upload_url(
+            vk_token, vk_api_version, group_id
         )
+        upload_params = vk.get_photo_upload_params(
+            upload_url, comic_path
+        )
+        attachments = vk.save_photo_on_server(
+            vk_token, vk_api_version, group_id, upload_params
+        )
+
         vk.publish_photo_on_wall(
             vk_token, vk_api_version, group_id,
             attachments, message=comic_alt
